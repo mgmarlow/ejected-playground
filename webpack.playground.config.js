bdc7d971cd4c4f4668d4c57764f1fe1c1355cd46
Klaus Kazlauskas <klausmachado@gmail.com>
2018-04-14 13:36:27 -0300

    add separated webpack config file for angular-playground

---
+++ webpack.playground.config.js
@@ -0,0 +1,20 @@
+var webpackMerge = require('webpack-merge');
+var webDev = require('./webpack.config');
+var mergeStrategy = {
+  customizeObject(a, b, key) {
+    if (key === 'entry') {
+      return Object.assign({}, a, b);
+    }
+
+    return undefined;
+  }
+};
+var webpackPlaygroundMerge = webpackMerge(mergeStrategy);
+
+module.exports = webpackPlaygroundMerge(webDev, {
+  "entry": {
+    "main": [
+      "./src/main.playground.ts"
+    ]
+  }
+});
